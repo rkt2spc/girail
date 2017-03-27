728dab7b5ee034791c450afb18d281190b7b3402
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-03-27 16:45:28 +0700

    Multi mailboxes cron

--- cron.js
+++ cron.js
@@ -1,53 +1,88 @@
 //========================================================================================================
+// External Depdendencies
 var async = require('async');
-var gmail = require('./gmail');
-var queue = require('./queue');
 
 //========================================================================================================
-gmail.retrieveUnprocessedMessages((err, messages) => {
-    if (err) return console.log(err);
-    if (messages.length === 0)
-        return console.log('Done, nothing to process');
+// Lib Depdendencies
+var configsAdapter = require('./lib/configs-adapter');
+var gmail = require('./lib/gmail');
+var queue = require('./lib/queue');
+var utils = require('./lib/utilities');
 
-    messages.reverse();
-    console.log(`Begin processing ${messages.length} messages`);
-    console.log('==================================================');
-    // Perform iterating asynchronously
-    async.eachSeries(
-        // Data source
-        messages,
-        // Iterating function
-        (message, next) => {
+//========================================================================================================
+// Configurations
+var gmailSettings = configsAdapter.loadGmailSettings();
+var mailboxSettings = configsAdapter.loadMailboxSettings();
 
-            console.log(`Processing message ${message.id}`);
-            queue.sendMessage(JSON.stringify(message), (err, data) => {
+//========================================================================================================
+// Cron process
+var mailboxes = gmail.generateMailboxes();
+async.eachSeries(mailboxes,
+    (mailbox, processNextMailbox) => {
 
-                // Can't enqueue
-                if (err) {
-                    console.log(err);
-                    next();
-                    return;
-                }
-
-                console.log(`Message ${message.id} enqueued`);
-                // Mark message enqueued
-                gmail.markMessageEnqueued(message, (err) => {
-                    
-                    if (err) {
-                        console.log(err);
-                        next();
-                        return;
-                    }
-
-                    console.log(`Message ${message.id} marked as enqueued`);
-                    next();
-                });
-            });
-        },
-        // Final callback
-        (err) => {
+        console.log("==============================================");
+        console.log(`Processing mailbox <${mailbox.name}>`);
+        mailbox.retrieveUnprocessedMessages((err, messages) => {
             if (err) return console.log(err);
-            console.log('Done, finished processing');
-        }
-    );
-});
+            if (messages.length === 0)
+                return console.log('Done, nothing to process');
+
+            messages.reverse();
+            console.log(`Begin processing ${messages.length} messages`);
+            // Perform iterating asynchronously
+            async.eachSeries(
+                // Data source
+                messages,
+                // Iterating function
+                (message, next) => {
+                    console.log('--------------------------------------------------');
+                    console.log(`Processing message ${message.id}`);
+                    mailbox.getMessage(message.id, (err, detailedMessage) => {
+
+                        if (err) {
+                            console.log('Failed to get message detail!');
+                            console.log(err);
+                            next();
+                            return;
+                        }
+
+                        queue.sendMessage(JSON.stringify(detailedMessage), (err, data) => {
+
+                            // Can't enqueue
+                            if (err) {
+                                console.log('Failed to enqueue message!');
+                                console.log(err);
+                                next();
+                                return;
+                            }
+
+                            console.log(`Message ${message.id} enqueued`);
+                            // Mark message enqueued
+                            mailbox.markMessageEnqueued(message, (err) => {
+
+                                if (err) {
+                                    console.log('Failed to mark message enqueued!');
+                                    console.log(err);
+                                    next();
+                                    return;
+                                }
+
+                                console.log(`Message ${message.id} marked as enqueued`);
+                                next();
+                            });
+                        });
+
+                    });
+                },
+                // Final callback
+                (err) => {
+                    if (err) return console.log(err);
+                    processNextMailbox();
+                }
+            );
+        });
+    },
+    (err) => {
+        if (err) return console.log(err.message);
+        console.log('Done, finished processing');
+    });
