fee4fdc3c1ab6e3db3aea471f04876d6134e0d47
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-05-29 17:46:06 +0700

    Cleanup even more syntax

--- cron-task.js
+++ cron-task.js
@@ -1,90 +1,83 @@
-module.exports = function (cronDone) {
+module.exports = (cronDone) => {
+  //------------------------------------------------------------------------------
+  // External Depdendencies
+  const async = require('async');
 
-    //========================================================================================================
-    // External Depdendencies
-    var async = require('async');
+  //------------------------------------------------------------------------------
+  // Lib Depdendencies
+  const gmail = require('./lib/gmail');
+  const queue = require('./lib/queue');
+  const logger = require('./lib/logger').cronLogger;
 
-    //========================================================================================================
-    // Lib Depdendencies
-    var configsAdapter = require('./lib/configs-adapter');
-    var utils = require('./lib/utilities');
-    var gmail = require('./lib/gmail');
-    var queue = require('./lib/queue');
-    var logger = require('./lib/logger').cronLogger;
+  //------------------------------------------------------------------------------
+  // Configurations
 
-    //========================================================================================================
-    // Configurations
-    var gmailSettings = configsAdapter.loadGmailSettings();
-    var mailboxSettings = configsAdapter.loadMailboxSettings();
+  //------------------------------------------------------------------------------
+  // Cron process
+  const mailboxes = gmail.generateMailboxes();
+  logger.info(`Processing ${mailboxes.length} mailboxes`);
+  async.eachSeries(mailboxes, (mailbox, processNextMailbox) => {
+    //-------------------------
+    logger.info('==============================================');
+    logger.info(`Processing mailbox <${mailbox.name}>`);
+    mailbox.retrieveUnprocessedMessages((err, messages) => {
+      //-------------------------
+      if (err) {
+        logger.info('Failed to retrieve unprocessed messages');
+        logger.error(err);
+        return processNextMailbox();
+      }
 
-    //========================================================================================================
-    // Cron process
-    var mailboxes = gmail.generateMailboxes();
-    logger.info(`Processing ${mailboxes.length} mailboxes`);
-    async.eachSeries(mailboxes,
-        (mailbox, processNextMailbox) => {
+      //-------------------------
+      if (messages.length === 0) {
+        logger.info(`Done, mailbox ${mailbox.name} has nothing to process`);
+        return processNextMailbox();
+      }
 
-            logger.info("==============================================");
-            logger.info(`Processing mailbox <${mailbox.name}>`);
-            mailbox.retrieveUnprocessedMessages((err, messages) => {
-                if (err) {
-                    logger.info('Failed to retrieve unprocessed messages');
-                    logger.error(err);
-                    return processNextMailbox();
-                }
-                if (messages.length === 0) {
-                    logger.info(`Done, mailbox ${mailbox.name} has nothing to process`);
-                    return processNextMailbox();
-                }
+      //-------------------------
+      messages.reverse();
+      logger.info(`Begin processing ${messages.length} messages`);
 
-                messages.reverse();
-                logger.info(`Begin processing ${messages.length} messages`);
-                // Perform iterating asynchronously
-                async.eachSeries(
-                    // Data source
-                    messages,
-                    // Iterating function
-                    (message, next) => {
-                        logger.info('--------------------------------------------------');
-                        logger.info(`Processing message ${message.id}`);
-
-                        message.mailbox = mailbox.name;
-                        queue.sendMessage(JSON.stringify(message), (err, data) => {
-
-                            // Can't enqueue
-                            if (err) {
-                                logger.info('Failed to enqueue message!')
-                                logger.error(err);
-                                next();
-                                return;
-                            }
-
-                            logger.info(`Message ${message.id} enqueued`);
-                            // Mark message enqueued
-                            mailbox.markMessageEnqueued(message, (err) => {
-
-                                if (err) {
-                                    logger.info('Failed to mark message enqueued!');
-                                    logger.error(err);
-                                    next();
-                                    return;
-                                }
-
-                                logger.info(`Message ${message.id} marked as enqueued`);
-                                next();
-                            });
-                        });
-                    },
-                    // Final callback
-                    (err) => {
-                        if (err) logger.error(err);
-                        processNextMailbox();
-                    }
-                );
-            });
-        },
-        (err) => {
-            if (err) logger.error(err);
-            cronDone();
+      //-------------------------
+      // Perform iterating asynchronously
+      async.eachSeries(
+      // Data source
+      messages,
+      // Iterating function
+      (message, next) => {
+        logger.info('--------------------------------------------------');
+        logger.info(`Processing message ${message.id}`);
+        message.mailbox = mailbox.name;
+        queue.sendMessage(JSON.stringify(message), (err, data) => {
+          // Can't enqueue
+          if (err) {
+            logger.info('Failed to enqueue message!')
+            logger.error(err);
+            next();
+            return;
+          }
+          logger.info(`Message ${message.id} enqueued`);
+          // Mark message enqueued
+          mailbox.markMessageEnqueued(message, (err) => {
+            if (err) {
+              logger.info('Failed to mark message enqueued!');
+              logger.error(err);
+              next();
+              return;
+            }
+            logger.info(`Message ${message.id} marked as enqueued`);
+            next();
+          });
         });
+      },
+      // Final callback
+      (err2) => {
+        if (err2) logger.error(err2);
+        processNextMailbox();
+      });
+    });
+  }, (err) => {
+    if (err) logger.error(err);
+    cronDone();
+  });
 };
--- cron.js
+++ cron.js
@@ -1,11 +1,17 @@
-var async = require('async');
-var cronTask = require('./cron-task');
-var stop = false;
+//------------------------------------------------------------------------------
+const async = require('async');
+const cronTask = require('./cron-task');
+let stop = false;
 
+//------------------------------------------------------------------------------
 async.forever((next) => {
-    if (stop) return next('Stopped');
-    // After a Task done, wait 60 sec before doing next task
-    cronTask(() => setTimeout(() => next(), 30000));
+  if (stop) return next('Stopped');
+
+  // After a Task done, wait 60 sec before doing next task
+  cronTask(() => setTimeout(() => next(), 30000));
 }, (err) => process.kill(process.pid, 'SIGUSR2'));
 
-process.once('SIGUSR2', () => stop = true);
+//------------------------------------------------------------------------------
+process.once('SIGUSR2', () => {
+  stop = true;
+});
