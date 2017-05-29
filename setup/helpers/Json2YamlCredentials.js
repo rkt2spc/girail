b0d02b9be36e350ab6c5c2d53d099adab345c6a8
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-05-29 17:29:42 +0700

    Cleanup syntax

--- setup/helpers/Json2YamlCredentials.js
+++ setup/helpers/Json2YamlCredentials.js
@@ -1,18 +1,18 @@
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Node Dependencies
 var fs = require('fs');
 var path = require('path');
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // External Dependencies
 var yargs = require('yargs');
 var yaml = require('js-yaml');
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Lib Dependencies
 var configsAdapter = require('./lib/configs-adapter');
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Configurations
 var argv = yargs
     .usage('Usage: $0 -m [string] -c [filepath]\n')
@@ -20,19 +20,19 @@ var argv = yargs
     .example('$0 --mailbox foo@mail.com --credentials /path/to/my/credentials.json')
     .alias('m', 'mailbox')
     .describe('m', 'Your mailbox name')
-    .alias('c', 'credentials')    
+    .alias('c', 'credentials')
     .describe('c', 'Path your google-json-credentials')
     .epilogue('This script parse your default google credentials in json and add it to our settings as yaml')
     .argv;
 
 var mailboxSettings = configsAdapter.loadMailboxSettings();
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Arguments
 var mailbox_name = argv.mailbox;
 var credentials_path = argv.credentials;
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Find mailbox
 var mailbox = mailboxSettings.find((mb) => mb.name === mailbox_name);
 if (!mailbox) {
@@ -40,10 +40,10 @@ if (!mailbox) {
     return;
 }
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Parse JSON Credentials
 try {
-    var credentials = Buffer.from(fs.readFileSync(path.resolve(credentials_path), 'utf8')).toString('base64');  
+    var credentials = Buffer.from(fs.readFileSync(path.resolve(credentials_path), 'utf8')).toString('base64');
 }
 catch (e) {
     console.log('Failed to parse json credentials');
@@ -51,7 +51,7 @@ catch (e) {
     return;
 }
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Update Settings
 mailbox.credentials = credentials;
 configsAdapter.updateMailboxSettings(mailboxSettings);
--- setup/mailbox.remove.js
+++ setup/mailbox.remove.js
@@ -1,10 +1,10 @@
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Node Dependencies
 var fs = require('fs');
 var path = require('path');
 var nativeUtil = require('util');
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // External Dependencies
 var yargs = require('yargs');
 var lodash = require('lodash');
@@ -13,18 +13,18 @@ var readlineSync = require('readline-sync');
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Lib Dependencies
 var utils = require('../lib/utilities');
 var configsAdapter = require('../lib/configs-adapter');
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Configurations
 var gmailSettings = configsAdapter.loadGmailSettings();
 var googleCredentials = configsAdapter.loadGoogleCredentials();
 var mailboxSettings = configsAdapter.loadMailboxSettings();
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 // Arguments
 var argv = yargs
     .usage('Usage: $0 -m [mailbox]')
@@ -36,7 +36,7 @@ var argv = yargs
     .describe('m', 'The mail box you want removed')
     .argv;
 
-//========================================================================================================
+//------------------------------------------------------------------------------
 console.log('\n===================================================');
 var mailbox = mailboxSettings.find((mb) => mb.name === argv.mailbox);
 
