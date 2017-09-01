89de4c0444aef4d77e2836738603ed84b870e77b
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-09-01 19:51:30 +0700

    Maintainence works

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,16 @@
 module.exports = {
-  "root": true,
   "env": {
+    "node": true,
     "commonjs": true,
     "es6": true,
-    "node": true
   },
   "extends": "airbnb-base",
-  "plugins": ["import"],
   "rules": {
-    "no-unused-vars": [
-      "error", {
-        "args": "none"
-      }
-    ],
-    "object-shorthand": [
-      "error", "consistent-as-needed"
-    ],
-    "func-names": [
-      "error", "as-needed"
-    ],
-    // "max-len": ["error", 120, 2, { ignoreComments: true }],
-    "max-len": "off",
+    // Generic Linting Rules
+    "no-unused-vars": ["error", { "args": "none" }],
+    "object-shorthand": ["error", "consistent-as-needed"],
+    "func-names": ["error", "as-needed"],
+    "max-len": ["error", 180, 2, { ignoreComments: true }],
     "no-plusplus": "off",
     "consistent-return": "off",
     "no-underscore-dangle": "off",
@@ -30,26 +20,23 @@ module.exports = {
     "prefer-template": "off",
     "arrow-parens": "off",
     "global-require": "off",
-    "comma-dangle": [
-      "error", {
-        "arrays": "always-multiline",
-        "objects": "always-multiline",
-        "imports": "always-multiline",
-        "exports": "always-multiline",
-        "functions": "ignore"
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "ignore"
+    }],
+    "key-spacing": ["error", {
+      "align": {
+        "beforeColon": true,
+        "afterColon": true,
+        "on": "colon",
+        "mode": "strict"
       }
-    ],
-    "key-spacing": [
-      "error", {
-        "align": {
-          "beforeColon": true,
-          "afterColon": true,
-          "on": "colon",
-          "mode": "strict"
-        }
-      }
-    ],
+    }],
+    // ES6 Linting Rules
     "import/no-dynamic-require": "off",
-    "import/newline-after-import": "off"
+    "import/newline-after-import": "off",
   }
 };
